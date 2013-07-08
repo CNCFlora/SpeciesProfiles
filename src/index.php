3a1101719808faa43e583cf346c0c6b351aecb4a
Diogo Souza <diogo@diogok.net>
2013-07-08 18:06:11 -0300

    fixed biblio interop to use elastic search

--- src/index.php
+++ src/index.php
@@ -42,7 +42,7 @@ $rest->addMap("GET","/",function($r) {
                 )
             )
         );
-        $r  = json_decode(file_get_contents(ES."/_search?source=".rawurlencode(json_encode($q))))->hits->hits;
+        $r  = json_decode(file_get_contents(ES."/profile/_search?source=".rawurlencode(json_encode($q))))->hits->hits;
         foreach($r as $rr) {
             $result[] = $rr->_source;
         }
@@ -366,14 +366,26 @@ $rest->addMap("GET","/work",function($r) {
 
 $rest->addMap("GET","/biblio",function($r) {
     $term=$r->getRequest()->getGet("term");
-    $start = rawurlencode($term);
-    $end = rawurlencode($term."99999999999999999");
-    $docs = Utils::$couchdb->getView("bibliography","short_citation",null,array('startkey'=>$start,'endkey'=>$end));
-    $r = array();
-    foreach($docs['rows'] as $row) {
-        $r[] = array("label"=>$row['value']['fullCitation'],'value'=>$row['value']['_id']);
+    $q = array(
+        "size"=>25,
+        "from"=>0,
+        "facets"=>new StdClass,
+        "query"=> array(
+            "constant_score"=> array(
+                "query"=> array(
+                    "query_string"=> array(
+                        "query"=> $term
+                    )
+                )
+            )
+        )
+    );
+    $r  = json_decode(file_get_contents(ES."/biblio/_search?source=".rawurlencode(json_encode($q))))->hits->hits;
+    $res = array();
+    foreach($r as $row) {
+        $res[] = array("label"=>$row->_source->fullCitation,'value'=>$row->_source->_id);
     }
-    return new \Rest\View\JSon($r);
+    return new \Rest\View\JSon($res);
 });
 
 
