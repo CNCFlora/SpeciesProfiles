7d2b1dad12455e73ce796f7273e045612f341999
Diogo Souza <diogo@diogok.net>
2013-10-15 16:32:49 -0300

    fix occurrence validation link, reference search

--- src/cncflora/controller/Search.php
+++ src/cncflora/controller/Search.php
@@ -11,21 +11,7 @@ class Search implements \Rest\View {
         $query  = null;
         if(isset($_GET["query"])) {
             $query = $r->GetRequest()->getGet("query");
-            $q = array(
-                "size"=>25,
-                "from"=>0,
-                "facets"=>new \StdClass,
-                "query"=> array(
-                    "constant_score"=> array(
-                        "query"=> array(
-                            "query_string"=> array(
-                                "query"=> $query
-                            )
-                        )
-                    )
-                )
-            );
-            $r  = json_decode(file_get_contents(ES."/profile/_search?source=".rawurlencode(json_encode($q))))->hits->hits;
+            $r  = json_decode(file_get_contents(ES."/profile/_search?q=".rawurlencode($query)))->hits->hits;
             foreach($r as $rr) {
                 $result[] = $rr->_source;
             }
@@ -35,21 +21,7 @@ class Search implements \Rest\View {
 
     public function biblio($r) {
         $term=$r->getRequest()->getGet("term");
-        $q = array(
-            "size"=>25,
-            "from"=>0,
-            "facets"=>new \StdClass,
-            "query"=> array(
-                "constant_score"=> array(
-                    "query"=> array(
-                        "query_string"=> array(
-                            "query"=> $term
-                        )
-                    )
-                )
-            )
-        );
-        $r  = json_decode(file_get_contents(ES."/biblio/_search?source=".rawurlencode(json_encode($q))))->hits->hits;
+        $r  = json_decode(file_get_contents(ES."/biblio/_search?q=".rawurlencode($term)))->hits->hits;
         $res = array();
         foreach($r as $row) {
             $res[] = array("label"=>$row->_source->fullCitation,'value'=>$row->_source->_id);
